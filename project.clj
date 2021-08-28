fae7ecccc554cc6e3a293c0bf60e857ee0fe972e
Sergey Terentyev <sergejs.terentjevs@gmail.com>
2021-08-29 00:12:15 +0100

    Upgraded Clojure version

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "https://github.com/aneksamun/speaking-clock"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.10.3"]]
   :aot [co.uk.redpixel.clock.application]
   :main co.uk.redpixel.clock.application)
